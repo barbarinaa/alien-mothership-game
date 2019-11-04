73f229bebfeaae06366f2bd6d99f0f59e458988a
Barbara Skender <barbee.skender@gmail.com>
2019-11-04 12:05:19 +0000

    Fixed the code so the hitpoints are visible

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-//Pseudocode//
 
-//create a button x
-//button on click targets random element x
+//create a button
+//button on click targets random element
 
-//create a Ship class (common traits: each have hitPoints, each can take damage, each die when hitpoint 0) x
-//Ship class has hitPoint variable x
-//create Queen, Drones, Worker subclass which inherit the ship class instances x
+//create a Ship class (common traits: each have hitPoints, each can take damage, each die when hitpoint 0)
+//create Queen, Drones, Worker subclass which inherit the ship class instances
 
-//Queen subclass has a hitPoint variable which has maximum of 80 x
-//Queen subclass has damage() which deducts 7 from hitPoint x
-// if hitPoint == 0 then game over; x
+//Queen subclass has a hitPoint variable which has maximum of 80
+//Queen subclass has damage() which deducts 7 from hitPoint
+// if hitPoint == 0 then game over;
 
-////Drone subclass has a hitPoint variable which has maximum of 60 x
-//Drone subclass has damage() which deducts 12 from hitPoint x
-// if hitPoint == 0 then the element with the class is unreachable/can't be hit; x
+////Drone subclass has a hitPoint variable which has maximum of 60
+//Drone subclass has damage() which deducts 12 from hitPoint
+// if hitPoint == 0 then the element with the class is unreachable/can't be hit;
 
-//Worker subclass has a hitPoint variable which has maximum of 68 x
-//Worker subclass has damage() which deducts 10 from hitpoint x
-// if hitPoint == 0 then the element with the class is unreachable/can't be hit; x
+//Worker subclass has a hitPoint variable which has maximum of 68
+//Worker subclass has damage() which deducts 10 from hitpoint
+// if hitPoint == 0 then the element with the class is unreachable/can't be hit;
 
 //Game over when all elements with queen, worker, drone classes = 0 / queen dead x
 
@@ -71,58 +69,37 @@ class Drone extends MainShip {
   damage = 12;
 }
 
-const addAliens = () => {
-  const alienArray = [];
-  const numWorkers = 5;
-  const numDrones = 8;
+const onlyQueen = new Queen();
+const worker1 = new Worker();
+const worker2 = new Worker();
+const worker3 = new Worker();
+const worker4 = new Worker();
+const worker5 = new Worker();
+const drone1 = new Drone();
+const drone2 = new Drone();
+const drone3 = new Drone();
+const drone4 = new Drone();
+const drone5 = new Drone();
+const drone6 = new Drone();
+const drone7 = new Drone();
+const drone8 = new Drone();
 
-  const queen = new Queen();
-  alienArray.push(queen);
-
-  for (let i = 0; i < numWorkers; i++) {
-    const worker = new Worker();
-    alienArray.push(worker);
-  }
-
-  for (let i = 0; i < numDrones; i++) {
-    const drone = new Drone();
-    alienArray.push(drone);
-  }
-};
-
-const ships = addAliens();
-
-// const onlyQueen = new Queen();
-// const worker1 = new Worker();
-// const worker2 = new Worker();
-// const worker3 = new Worker();
-// const worker4 = new Worker();
-// const worker5 = new Worker();
-// const drone1 = new Drone();
-// const drone2 = new Drone();
-// const drone3 = new Drone();
-// const drone4 = new Drone();
-// const drone5 = new Drone();
-// const drone6 = new Drone();
-// const drone7 = new Drone();
-// const drone8 = new Drone();
-
-// const ships = [
-//   onlyQueen,
-//   worker1,
-//   worker2,
-//   worker3,
-//   worker4,
-//   worker5,
-//   drone1,
-//   drone2,
-//   drone3,
-//   drone4,
-//   drone5,
-//   drone6,
-//   drone7,
-//   drone8
-// ];
+const ships = [
+  onlyQueen,
+  worker1,
+  worker2,
+  worker3,
+  worker4,
+  worker5,
+  drone1,
+  drone2,
+  drone3,
+  drone4,
+  drone5,
+  drone6,
+  drone7,
+  drone8
+];
 
 const button = document.getElementById("button");
 
@@ -138,7 +115,7 @@ const doDamage = () => {
 button.onclick = doDamage;
 
 const showHitPoint = () => {
-  document.getElementById("queen").innerHTML = `&#10084;${ship[targetIndex].hitPoints}`;
+  document.getElementById("queen").innerHTML = `&#10084;${onlyQueen.hitPoints}`;
   document.getElementById("worker1").innerHTML = `&#10084;${worker1.hitPoints}`;
   document.getElementById("worker2").innerHTML = `&#10084;${worker2.hitPoints}`;
   document.getElementById("worker3").innerHTML = `&#10084;${worker3.hitPoints}`;
